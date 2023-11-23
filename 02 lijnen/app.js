4d2efbdcb8cf9ce2d602c3d397ec311d180d01fb
Romy <mail@romy.dev>
2023-11-23 14:55:52 +0100

    canvas

---
+++ 02 lijnen/app.js
@@ -0,0 +1,21 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello world!");
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        console.log(canvas);
+        g.beginPath();
+        g.fillStyle = "pink";
+        g.moveTo(20,20);
+        g.lineTo(300,300);
+        g.lineTo(580,20);
+        g.closePath();
+        g.stroke();
+        g.fill();
+    }
+}
+
+let app = new App();
+app.runApplication();
