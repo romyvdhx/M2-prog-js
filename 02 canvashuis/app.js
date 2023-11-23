fe112ba93855ba78a231f4045157058fe6a0d41e
Romy <mail@romy.dev>
2023-11-23 14:44:53 +0100

    kleurtje

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -6,6 +6,9 @@ class App
         let canvas = document.getElementById("canvasId");
         let g = canvas.getContext("2d");
         console.log(canvas);
+        g.fillStyle = "pink";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "purple";
         g.fillRect(0,0,10,10);
     }
 }
