95d73427966626f65b68fdbad2fa342acccb07b2
Romy <mail@romy.dev>
2023-11-28 14:13:41 +0100

    les 3 opdr 1

---
+++ 03 ifelse/app.js
@@ -0,0 +1,12 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello world!");
+        //document.getElementById("newstitle");
+        //document.getElementByClass("gamenews");
+    }
+}
+
+let app = new App();
+app.runApplication();
