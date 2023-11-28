2618c540bef23ffba471bdd943426fb8ade2ea6c
Romy <mail@romy.dev>
2023-11-28 14:42:23 +0100

    les 3 opdr 2

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -4,7 +4,59 @@ class App
     {
         console.log("Hello world!");
         //document.getElementById("newstitle");
-        //document.getElementByClass("gamenews");
+        //document.getElementsByClassName("gamenews");
+        //document.getElementsByClassName("gamenews headline");
+        let title = document.getElementById("newstitle");
+        console.log(title);
+
+        let random = Math.random();
+        console.log(random);
+        if(random < 0.2)
+        {
+            title.style.backgroundColor = "#FF0000";
+        } else if(random >= 0.2 && random < 0.6)
+        {
+            title.style.backgroundColor = "#FF00FF";
+        } else if(random >= 0.6 && random < 0.75)
+        {
+            title.style.backgroundColor = "#800000";
+        } else 
+        {
+            title.style.backgroundColor = "#00FF00";
+        }
+
+        let newsitem1 = document.getElementsByClassName("gamenews")[1];
+        let newsitem2 = document.getElementsByClassName("gamenews headline")[0];
+
+        console.log(newsitem1);
+        if(random < 0.2)
+        {
+            newsitem1.style.backgroundColor = "#FF0000";
+        } else if(random >= 0.2 && random < 0.6)
+        {
+            newsitem1.style.backgroundColor = "#FF00FF";
+        } else if(random >= 0.6 && random < 0.75)
+        {
+            newsitem1.style.backgroundColor = "#800000";
+        } else 
+        {
+            newsitem1.style.backgroundColor = "#00FF00";
+        }
+
+        console.log(newsitem2);
+        if(random < 0.2)
+        {
+            newsitem2.style.backgroundColor = "#FF0000";
+        } else if(random >= 0.2 && random < 0.6)
+        {
+            newsitem2.style.backgroundColor = "#FF00FF";
+        } else if(random >= 0.6 && random < 0.75)
+        {
+            newsitem2.style.backgroundColor = "#800000";
+        } else 
+        {
+            newsitem2.style.backgroundColor = "#00FF00";
+        }
     }
 }
 
