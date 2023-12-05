f4231ec8ed9be1d2c0cbbc634ea931375b50022f
Romy <mail@romy.dev>
2023-12-05 14:42:54 +0100

    les 2 opdr 3

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -10,6 +10,24 @@ function argumentsAreHandy(shoutout)
     console.log(shoutout);
 }
 
+function heeftEenResultaat()
+{
+    let resultaat = 1;
+    return resultaat;
+}
+
+function ax2bcWiskunde(x, a, b, c)
+{
+    let y = (a*(x*x)) + (b*x) + c;
+    return y;
+}
+
+function wiskunde(u, h, l)
+{
+    let logY = Math.log(l) + h + Math.pow(u, 2);
+    return logY;
+}
+
 superMooieGlobalFunction();
 superMooieGlobalFunction();
 superMooieGlobalFunction();
@@ -18,6 +36,29 @@ argumentsAreHandy("Super Shout Out!");
 argumentsAreHandy("Mega Shout Out!");
 argumentsAreHandy("Gigantic Shout Out!");
 
+let hetResultaat = heeftEenResultaat();
+console.log(hetResultaat);
+console.log(heeftEenResultaat());
+
+//let x = 9;
+//let a = 3;
+//let b = 4;
+//let c = 89;
+
+//let y = (a*(x*x)) + (b*x) + c;
+//console.log(y);
+
+let y1 = ax2bcWiskunde(9,3,4,89);
+console.log(y1);
+
+let y2 = ax2bcWiskunde(3,6,5,45);
+console.log(y2);
+
+let y3 = ax2bcWiskunde(7,1,2,13);
+console.log(y3);
+
+let y4 = wiskunde(9,8,15);
+console.log(y4);
 
 class App
 {
