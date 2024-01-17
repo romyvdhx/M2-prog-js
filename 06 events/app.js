aed6377664748f769e87a6ecf77a1327908f1d21
Romy <mail@romy.dev>
2024-01-17 20:26:55 +0100

    les 6 opdr 3

--- 06 events/app.js
+++ 06 events/app.js
@@ -6,7 +6,43 @@ class App
         let uiButton = document.getElementById("myButton");
         uiButton.addEventListener("click", (e)=>
         {
-            console.log("click!");
+            const para = document.createElement("p");
+            const node = document.createTextNode("This is new.");
+            para.appendChild(node);
+            document.body.appendChild(para);
+        });
+        let newButton = document.getElementById("newButton");
+        newButton.addEventListener("click", (e)=>
+        {
+            const para1 = document.createElement("h1");
+            const node1 = document.createTextNode("It'sa me!");
+            para1.appendChild(node1);
+            document.body.appendChild(para1);
+            const para2 = document.createElement("p");
+            const node2 = document.createTextNode("Mario");
+            para2.appendChild(node2);
+            document.body.appendChild(para2);
+            let link = document.createElement("a");
+            link.href = "https://www.iphoned.nl/wp-content/uploads/2022/03/mario-dag.jpg";
+            link.alt = "A picture of Mario waving.";
+            const para3 = document.createElement("img");
+            para3.src = "mario.jpg";
+            link.appendChild(para3);
+            document.body.appendChild(link);
+        });
+        let button = document.getElementById("button");
+        button.style.backgroundColor = "#7FFFD4";
+        let pButton = document.getElementById("button");
+        pButton.addEventListener("click", (e)=>
+        {
+            const para1 = document.createElement("h1");
+            const node1 = document.createTextNode("clicked a div");
+            para1.appendChild(node1);
+            document.body.appendChild(para1);
+            const para2 = document.createElement("p");
+            const node2 = document.createTextNode("wow!");
+            para2.appendChild(node2);
+            document.body.appendChild(para2);
         });
     }
 }
