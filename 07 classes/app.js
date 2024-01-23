2ed4eb9b18d71ba9dca9c8bb7e2390dbf1858f85
Romy <mail@romy.dev>
2024-01-23 10:34:27 +0100

    les 7 opdr 1

---
+++ 07 classes/app.js
@@ -0,0 +1,33 @@
+class Greet 
+{
+    constructor()
+    {
+        console.log("Greetings!");
+    }
+}
+
+let greet = new Greet();
+new Greet();
+new Greet();
+new Greet();
+
+class Goodbye
+{
+    constructor()
+    {
+        console.log("Bye Bye Bye");
+    }
+}
+
+let bye = new Goodbye();
+
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+    }
+}
+
+let app = new App();
+app.runApplication();
