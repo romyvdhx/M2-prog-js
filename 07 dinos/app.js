bd44e14bb90670e91fce5b8fdbf917ea1b9a26d5
Romy <mail@romy.dev>
2024-01-24 09:29:37 +0100

    les 7 opdr 5

--- 07 dinos/app.js
+++ 07 dinos/app.js
@@ -5,6 +5,16 @@ class Dino
         this.naam = naam;
         this.vleeseter = vleeseter;
         this.leeftijd = leeftijd;
+        this.leeft = true;
+        this.honger = true;
+    }
+
+    eatFood(foodToEat)
+    {
+        foodToEat.leeft = false;
+        this.honger = false;
+        console.log("ik ben een " + this.naam);
+        console.log("mijn honger: " + this.honger);
     }
 }
 
@@ -15,8 +25,14 @@ class App
         console.log("Hello world!");
         
         let dino = new Dino("T-Rex", true, 20);
+        let plantenEter = new Dino("Triceratops", false, 20);
+        console.log(dino);
+        console.log(plantenEter);
         console.log("de leeftijd van deze " + dino.naam + " is: " + dino.leeftijd);
         console.log("en deze " + dino.naam + " eet vlees: " + dino.vleeseter);
+
+        dino.eatFood(plantenEter);
+        console.log("leeft " + plantenEter.naam + "? " + plantenEter.leeft);
     }
 }
 
